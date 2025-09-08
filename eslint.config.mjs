62ace1929a9ac3b73130d254773684868517e508
jmchavarria <marlon05chavarria@gmail.com>
2025-09-07 22:49:17 -0500

    feat: actualizar configuración de ESLint para desactivar durante el build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,10 @@
-import { FlatCompat } from '@eslint/eslintrc'
- 
-const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-})
- 
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
-  }),
-]
- 
-export default eslintConfig
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true }, // ⬅️ desactiva ESLint en el build
+  // Si quieres, también puedes desactivar TypeScript en build, pero NO lo recomiendo:
+  // typescript: { ignoreBuildErrors: true },
+  experimental: {},
+};
+
+export default nextConfig;
