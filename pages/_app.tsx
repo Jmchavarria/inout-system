418dd9359a007f12e834b7084032f5bf9b58853d
jmchavarria <marlon05chavarria@gmail.com>
2025-09-21 15:52:24 -0500

    n4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -139,20 +139,32 @@ const AppLayout = ({ page }: { page: ReactElement }) => {
   return (
     <div className='flex h-screen overflow-hidden'>
       <Sidebar />
-        <main className='flex-1 overflow-y-auto bg-gray-50 px-4 py-2'>
+      <main className='flex-1 overflow-y-auto bg-gray-50 px-4 py-2'>
         <Topbar />
-          <div className='max-w-7xl mx-auto'>
-            {page}
-          </div>
-        </main>
+        <div className='max-w-7xl mx-auto'>
+          {page}
+        </div>
+      </main>
     </div>
   );
 };
-const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+
+// Componente interno que usa el layout con acceso al contexto
+const AppWithLayout = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => <AppLayout page={page} />);
 
+  return getLayout(<Component {...pageProps} />);
+};
 
-  return <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>;
+// Componente principal que provee el contexto
+const MyApp = ({ Component, pageProps, ...rest }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => <AppLayout page={page} />);
+
+  return (
+    <AuthProvider>
+      {getLayout(<Component {...pageProps} />)}
+    </AuthProvider>
+  );
 };
 
 export default MyApp;
